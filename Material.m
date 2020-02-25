806ae73fe270034b826b04c8f002f23a521498ec
EduardGomezEscandell <27deMaig>
2020-02-25 12:22:32 +0100

    Added plain strain in Material class

--- Material.m
+++ Material.m
@@ -14,12 +14,20 @@ classdef Material < handle
         end
         
         function obj = calcConstitutive(obj, problemType)
+            
             if(problemType == 1) % 1 -- Plane stress
                 obj.D = [ 1  obj.v 0;
                         obj.v  1   0;
                         0      0  1-obj.v];
                 obj.D = obj.E / (1 - obj.v^2) * obj.D;
+                
+            elseif(problemType == 2) % 2 -- Plain strain
+                obj.D = [ 1-obj.v    obj.v   0;
+                           obj.v   1-obj.v   0;
+                             0         0  1-2*obj.v];
+                obj.D = obj.E / (1 + obj.v) / (1 - 2*obj.v) * obj.D;
             end
+            
         end
     end
 end
