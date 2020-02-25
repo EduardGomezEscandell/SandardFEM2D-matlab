85bb472133c6c106ea2a751f668fb025a852e230
EduardGomezEscandell <27deMaig>
2020-02-25 12:31:56 +0100

    Added Boundary Condition to Node class

--- Node.m
+++ Node.m
@@ -2,11 +2,15 @@ classdef Node < handle
    properties
       id
       X
+      BC_type
+      BC_value
    end
    methods
-       function obj = Node(id, X)
+       function obj = Node(id, X, n_DOF)
            obj.id = id;
            obj.X = X;
+           obj.BC_type = repmat('N',1,n_DOF); % BC by defalult Neumann=0
+           obj.BC_value = zeros(1,n_DOF);
        end
    end
 end
