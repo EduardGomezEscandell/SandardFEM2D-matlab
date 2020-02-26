de967b8ca6a36972aefb14d08930c23696961d30
EduardGomezEscandell <eduard.gomez.escandell@gmail.com>
2020-02-26 09:35:50 +0100

    Started main file

---
+++ main.m
@@ -0,0 +1,15 @@
+% Data entry
+inputFileName = 'data/square';
+outputFileName = 'results/square.res';
+
+% Loading geomery
+dom = Domain();
+dom.loadFromFile(inputFileName);
+
+% Loading math data
+
+% Assembling system
+
+% Solving
+
+% Post-processing
