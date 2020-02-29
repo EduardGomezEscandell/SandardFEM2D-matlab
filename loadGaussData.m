1f4e87875c68bbd97deda72424c17e786473093d
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-02-29 20:02:49 +0100

    Added support for triangular gauss quadrature

--- loadGaussData.m
+++ loadGaussData.m
@@ -2,8 +2,8 @@ function gaussData = loadGaussData(dom)
     if(dom.n_dimensions == 1)
         gaussData = loadGaussSegment(dom.integrationDegree);
     elseif(dom.n_dimensions == 2)
-        if(dom.nodes_per_elem == 3)
-            gaussData = loadGaussTriangle(integrationDegree);
+        if(dom.elem_type == 'T')
+            gaussData = loadGaussTriangle(dom);
         end
         % else  --> Quads
     end
@@ -22,7 +22,7 @@ function gaussData = loadGaussSegment(integrationDegree)
     target = ['n = ',num2str(integrationDegree+1)];
     found = false;
     line = fgetl(fileGauss);
-    while ~strcmp(line,'End File')
+    while ~feof(fileGauss)
         line = fgetl(fileGauss);
         if strcmp(line, target)
             found = true;
@@ -39,6 +39,51 @@ function gaussData = loadGaussSegment(integrationDegree)
     
     % Loading data
     for i=1:integrationDegree+1
+        line = fgetl(fileGauss);
+        data = split(line);
+        w = str2double(data(2));
+        z = str2double(data(3));
+        gaussData{i} = Gauss_point(w, z);
+    end
+    
+    % Closing file
+    fclose(fileGauss);
+end
+
+function gaussData = loadGaussTriangle(domain)
+    % Loading file
+    fileGaussName = 'math_data/gauss_points_triangle.txt';
+    fileGauss = fopen(fileGaussName);
+    if fileGauss<0
+       error(['Failed to find file ', fileGaussName]);
+    end
+    
+    % Finding integration degree
+    target = ['n = ',num2str(domain.integrationDegree+1)];
+    found = false;
+    line = fgetl(fileGauss);
+    while ~feof(fileGauss)
+        line = fgetl(fileGauss);
+        data = split(line);
+        if strcmp(data{1},'n') && strcmp(data{2},'=')
+            n_points = str2double(data{3});
+            if n_points >= domain.integrationDegree+1
+                found = true;
+                break;
+            end
+        end
+    end
+    
+    if ~found
+        fclose(fileGauss);
+        error(['Failed to retrieve Gauss points in segment of order ',num2str(domain.integrationDegree)]);
+    end
+    
+    gaussData = cell(n_points,1);
+    domain.integrationDegree = n_points-1;
+    
+    % Loading data
+    for i=1:n_points
         line = fgetl(fileGauss);
         data = split(line);
         w = str2double(data(2));
