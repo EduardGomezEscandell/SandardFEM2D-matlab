2e69510bff538d035821ea6fc16b877f5eaf3007
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-03-03 23:21:04 +0100

    Created python file to generate boundary conditions file

---
+++ other/create_BC_file.py
@@ -0,0 +1,29 @@
+file_in = open('mesh.msh','r')
+file_ou = open('boundaries.txt','w+')
+
+file_ou.write('Dirichlet\n')
+
+coordinates = False
+
+for line in file_in:
+	
+	if not coordinates:
+		if line == 'Coordinates\n':
+			coordinates = True
+		continue
+	if line == 'End Coordinates\n':
+		break
+
+	data = line.split()
+	X = []
+	for i in range(3):
+		X.append(float(data[1+i]))
+	if(X[1] == 0 ):
+		file_ou.write('%6d   %10.5f\n'%(int(data[0]), 0))
+	elif(X[1]==1):
+		file_ou.write('%6d   %10.5f\n'%(int(data[0]), 20))
+
+file_ou.write('End Dirichlet\n')
+
+file_in.close()
+file_ou.close()
