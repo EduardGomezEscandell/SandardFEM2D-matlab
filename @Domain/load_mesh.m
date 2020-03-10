cb5f241bd094ba312966156e30e82b634bbda4b9
EduardGomezEscandell <eduard.gomez.escandell@gmail.com>
2020-03-10 12:24:06 +0100

    Changes to allow reading edges

--- @Domain/load_mesh.m
+++ @Domain/load_mesh.m
@@ -1,14 +1,14 @@
 function load_mesh(obj, project_dir)
      % Reads the mesh file. Called from readFromFile
 
-    % Opening file
-    meshFileName = [project_dir,'/mesh.msh'];
+    %%  Opening file
+    meshFileName = [project_dir,'/mesh.fmsh'];
     meshFile = fopen(meshFileName);
     if(meshFile < 1)
         error(['Failed to find file ',meshFileName]);
     end
 
-    % Various attributes
+    %%  Various attributes
     line = fgetl(meshFile);
     data = split(line);
 
@@ -23,6 +23,7 @@ function load_mesh(obj, project_dir)
         obj.interpolationDegree = obj.nodes_per_elem / 4;
     end
 
+    %% Nodes
     % Finding start of node list
     line = fgetl(meshFile);
     while(~strcmp(line,'Coordinates'))
@@ -47,29 +48,81 @@ function load_mesh(obj, project_dir)
 
         line = fgetl(meshFile);
     end
-
+    
+    %% Element-nodes
     % Finding start of element list
     line = fgetl(meshFile);
-    while(~strcmp(line,'Elements'))
+    while(~strcmp(line,'Elements-nodes'))
         line = fgetl(meshFile);
     end
 
     % Running through elements
     line = fgetl(meshFile);
-    while(~strcmp(line,'End Elements'))
+    while(~strcmp(line,'End Elements-nodes'))
         data = split(line);
 
         if size(data,2) == 0
             line = fgetl(meshFile);
             continue
         end
-
-        node_IDs = zeros(1,obj.nodes_per_elem);
-        for i=1:obj.nodes_per_elem
-            node_IDs(i) = str2double(data{1+i});
+        
+        elem_id = str2double(data{1});
+        node_IDs = [];
+        for i=2:obj.nodes_per_elem
+            node_IDs(end+1) = str2double(data{i});
         end
+        
+        obj.elems{i}.add_nodes(node_IDs);
+        line = fgetl(meshFile);
+    end
+    
+    %% Edges
+    % Finding start of edge list
+    line = fgetl(meshFile);
+    while(~strcmp(line,'Edges'))
+        line = fgetl(meshFile);
+    end
+    
+    line = fgetl(meshFile);
+    while(~strcmp(line,'End Edges'))
+        data = split(line);
 
-        obj.new_elem(node_IDs);
+        if size(data,2) == 0
+            line = fgetl(meshFile);
+            continue
+        end
+        
+        node_IDs(2) = str2double(data{3});
+        node_IDs(1) = str2double(data{2});
+        obj.new_edge(node_IDs);
+        
+        line = fgetl(meshFile);
+    end
+    
+    %% Elements-edges
+    % Finding start of element list
+    line = fgetl(meshFile);
+    while(~strcmp(line,'Elements-edges'))
+        line = fgetl(meshFile);
+    end
+
+    % Running through elements
+    line = fgetl(meshFile);
+    while(~strcmp(line,'End Elements-edges'))
+        data = split(line);
+
+        if size(data,2) == 0
+            line = fgetl(meshFile);
+            continue
+        end
+        
+        edge_IDs = [];
+        
+        for i=2:size(data,2)
+            edge_IDs(end+1) = str2double(data{i});
+        end
+        
+        obj.new_elem(edge_IDs);
 
         line = fgetl(meshFile);
     end
